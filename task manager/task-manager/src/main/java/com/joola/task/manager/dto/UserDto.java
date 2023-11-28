3a797c91f93936ed689e91c88538600bbda37c29
fatemehjj <joolafatemeh652@gmail.com>
2023-11-28 23:53:20 +0330

    add some basic validations in dto

--- task manager/task-manager/src/main/java/com/joola/task/manager/dto/UserDto.java
+++ task manager/task-manager/src/main/java/com/joola/task/manager/dto/UserDto.java
@@ -1,12 +1,24 @@
 package com.joola.task.manager.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.crypto.Mac;
+
 @Data
 public class UserDto {
+    @NotEmpty(message = "task cant be empty")
+    @NotBlank(message = "task cant be blank")
+    @Size(min = 3, max = 35)
     String task;
+    @NotNull
     int year;
+    @NotBlank(message = "task cant be blank")
+    @NotEmpty(message = "task cant be empty")
+    @Size(min = 3, max = 9)
     String month;
+    @NotNull
     int day;
     String task_state;
 }
